71e893c2f967e4e03af3a053bb4eb2ed131d5552
maesterfox <talentedmrfox@hotmail.com>
2024-04-30 12:47:28 +0100

    adding projects

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,15 +1,21 @@
 import React from "react";
+import foxLogo from "../public/logo.png";
 
 export default function Footer() {
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
+      <img
+        src={foxLogo.src} // Use .src for the path
+        alt="David Fox Logo"
+        className="h-8 w-10 inline-block mb-2"
+      />
       <small className="mb-2 block text-xs">
         &copy; 2024 David Fox. All rights reserved.
       </small>
       <p className="text-xs">
         <span className="font-semibold">About this website:</span> built with
-        React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS,
-        Framer Motion, React Email & Resend, Vercel hosting.
+        React & Next.js, TypeScript, Tailwind CSS, Framer Motion, and hosted on
+        Vercel.
       </p>
     </footer>
   );
--- lib/data.ts
+++ lib/data.ts
@@ -2,8 +2,8 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 // import { FaReact } from "react-icons/fa";
 // import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
+import foxmgmt from "@/public/foxmgmt.jpg";
+import foxexpense from "@/public/foxexpense.jpg";
 import wordanalyticsImg from "@/public/wordanalytics.png";
 
 export const links = [
@@ -121,25 +121,19 @@ export const experiencesData: ExperiencesData = [
 
 export const projectsData = [
   {
-    title: "CorpComment",
+    title: "Fox ProMGMT",
     description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
+      "Fox ProMGMT is a full-stack project management system built with GraphQL, Express, MongoDB, React, and Apollo. It provides a comprehensive platform for managing projects, clients, and tasks efficiently.",
+    tags: ["GraphQL", "Express", "MongoDB", "React", "Apollo"],
+    imageUrl: foxmgmt,
   },
+
   {
-    title: "rmtDev",
+    title: "Expense Tracker App",
     description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
-  },
-  {
-    title: "Word Analytics",
-    description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
+      "Full-stack expense tracker application built with the MERN stack and Apollo GraphQL. Allows users to track expenses, manage budgets, and analyze spending habits.",
+    tags: ["MERN", "Apollo GraphQL", "Expense Tracker", "Full-stack"],
+    imageUrl: foxexpense,
   },
 ] as const;
 
