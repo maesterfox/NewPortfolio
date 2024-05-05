59dd40a60bc1c02ed00428966b054e6023606bba
maesterfox <talentedmrfox@hotmail.com>
2024-05-05 13:14:33 +0100

    styles, buttons

--- components/experience.tsx
+++ components/experience.tsx
@@ -1,3 +1,4 @@
+// Experience section
 "use client";
 
 import React from "react";
@@ -18,39 +19,48 @@ export default function Experience() {
   return (
     <section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
       <SectionHeading>My experience</SectionHeading>
-      <VerticalTimeline lineColor="">
+      <VerticalTimeline lineColor={theme === "light" ? "#ddd" : "#333"}>
         {experiencesData.map((item, index) => (
-          <React.Fragment key={index}>
-            <VerticalTimelineElement
-              contentStyle={{
-                background:
-                  theme === "light" ? "#f3f4f6" : "rgba(255, 255, 255, 0.05)",
-                boxShadow: "none",
-                border: "1px solid rgba(0, 0, 0, 0.05)",
-                textAlign: "left",
-                padding: "1.3rem 2rem",
-              }}
-              contentArrowStyle={{
-                borderRight:
-                  theme === "light"
-                    ? "0.4rem solid #9ca3af"
-                    : "0.4rem solid rgba(255, 255, 255, 0.5)",
-              }}
-              date={item.date}
-              icon={item.icon}
-              iconStyle={{
-                background:
-                  theme === "light" ? "white" : "rgba(255, 255, 255, 0.15)",
-                fontSize: "1.5rem",
-              }}
-            >
-              <h3 className="font-semibold capitalize">{item.title}</h3>
-              <p className="font-normal !mt-0">{item.location}</p>
-              <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">
-                {item.description}
-              </p>
-            </VerticalTimelineElement>
-          </React.Fragment>
+          <VerticalTimelineElement
+            key={index}
+            contentStyle={{
+              background:
+                theme === "light" ? "#f3f4f6" : "rgba(255, 255, 255, 0.05)",
+              boxShadow: "none",
+              border: "1px solid rgba(0, 0, 0, 0.05)",
+              textAlign: "left",
+              padding: "1.3rem 2rem",
+            }}
+            contentArrowStyle={{
+              borderRight:
+                theme === "light"
+                  ? "0.4rem solid #9ca3af"
+                  : "0.4rem solid rgba(255, 255, 255, 0.5)",
+            }}
+            date={item.date}
+            icon={item.icon}
+            iconStyle={{
+              background:
+                theme === "light" ? "white" : "rgba(255, 255, 255, 0.15)",
+              fontSize: "1.5rem",
+            }}
+          >
+            <h3 className="font-semibold capitalize">{item.title}</h3>
+            <p className="font-normal !mt-0">{item.location}</p>
+            <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">
+              {item.description}
+            </p>
+            <ul className="flex flex-wrap mt-4 gap-2">
+              {item.tech_stack.map((tech, techIndex) => (
+                <li
+                  className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
+                  key={techIndex}
+                >
+                  {tech}
+                </li>
+              ))}
+            </ul>
+          </VerticalTimelineElement>
         ))}
       </VerticalTimeline>
     </section>
