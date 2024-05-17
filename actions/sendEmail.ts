28b00dfdf309cf77d12c1af1a1440ba6a93ef587
maesterfox <talentedmrfox@hotmail.com>
2024-05-17 13:49:39 +0100

    resend email form creation

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -27,7 +27,7 @@ export const sendEmail = async (formData: FormData) => {
   try {
     data = await resend.emails.send({
       from: "onboarding@resend.dev",
-      to: "talentedmrfox@hotmail.com",
+      to: "david.fox@davidfoxdev.co.uk",
       subject: "Message from contact form",
       reply_to: senderEmail,
       react: React.createElement(ContactFormEmail, {
